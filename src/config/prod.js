0c455ca1ae30e43a28fc5f2503fafa84d8694b1b
Shay <shay291@gmail.com>
2020-10-27 11:59:38 +0200

    keys

---
+++ src/config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    BASE_URL: process.env.REACT_APP_BASE_URL,
+    API_KEY: process.env.REACT_APP_API_KEY,
+    AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN,
+    DATABASE_URL: process.env.REACT_APP_DATABASE_URL,
+    PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
+    STORAGE_BUCKET: process.env.REACT_APP_STORAGE_BUCKET,
+    APP_ID: process.env.REACT_APP_APP_ID,
+}
+
