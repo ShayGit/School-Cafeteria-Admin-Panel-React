080b1069df9b3ad4f5a7d4a071e1ae36aafd12e7
Shay <shay291@gmail.com>
2020-10-27 12:01:25 +0200

    prod

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,10 +1,10 @@
 module.exports = {
-    BASE_URL: process.env.REACT_APP_BASE_URL,
-    API_KEY: process.env.REACT_APP_API_KEY,
-    AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN,
-    DATABASE_URL: process.env.REACT_APP_DATABASE_URL,
-    PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
-    STORAGE_BUCKET: process.env.REACT_APP_STORAGE_BUCKET,
-    APP_ID: process.env.REACT_APP_APP_ID,
+    BASE_URL: process.env.BASE_URL,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    APP_ID: process.env.APP_ID,
 }
 
